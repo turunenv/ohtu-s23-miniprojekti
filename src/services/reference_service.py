51fb2ddd46c1aa7f51b80cec54643d1808576b03
turunenv <turunenv@outlook.com>
2023-11-21 17:16:25 +0200

    initiel reference service

---
+++ src/services/reference_service.py
@@ -0,0 +1,13 @@
+from entities.book_reference import BookReference
+
+class ReferenceService:
+    def __init__(self):
+        self.fields_from_type = {
+            "book": ["ref_key", "author", "title", "year", "publisher", ]
+        }
+
+    def get_fields_of_reference_type(self, type):
+        return self.fields_from_type[type]
+    
+    def create_reference(self, content):
+        pass
