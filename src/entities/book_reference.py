d26b52e204a5716f65954c8e85f9ccb9fa578dcb
turunenv <turunenv@outlook.com>
2023-11-21 16:37:02 +0200

    add book-reference to entities

---
+++ src/entities/book_reference.py
@@ -0,0 +1,17 @@
+class BookReference:
+    """Class that describes a reference of type Book
+
+        Mandatory arguments:
+            -ref_key, author, title, year, publisher
+        Optional arguments:
+            -editor, volume, pages
+    """
+    def __init__(self, ref_key, author, title, year, publisher, editor=None, volume=None, pages=None):
+        self.ref_key = ref_key
+        self.author = author
+        self.title = title
+        self.year = year
+        self.publisher = publisher
+        self.editor = editor
+        self.volume = volume
+        self.pages = pages
