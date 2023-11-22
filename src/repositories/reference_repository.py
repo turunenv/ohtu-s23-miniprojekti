da733ce92b3d4510c6952d88ed73780d391f7ca9
Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>
2023-11-22 17:17:09 +0200

    initial reference_repository

---
+++ src/repositories/reference_repository.py
@@ -0,0 +1,40 @@
+from database_connection import get_db_connection
+from entities.book_reference import BookReference
+
+class ReferenceRepository:
+
+    def __init__(self, connection):
+        self._connection = connection
+
+    def create_book(self, book):
+        """Saves book reference into database.
+
+        Args:
+            book (_type_): BookReference
+        """
+        cursor = self._connection.cursor()
+        cursor.execute(
+            "INSERT INTO books (ref_key, author, title, year, publisher) VALUES (?, ?)"
+            (book.ref_key, book.author, book.title, book.year, book.publisher)
+        )
+
+        self._connection.commit()
+
+
+    def get_all(self):
+        """Finds all references from database.
+
+        Returns:
+            Returns a list of BookReferences
+        """
+        cursor = self._connection.cursor()
+        cursor.execute("SELECT * from books")
+
+        books = cursor.fetchall()
+
+        books_list = []
+
+        for book in books:
+            books_list.append(BookReference(book[0], book[1], book[2], book[3], book[4]))
+
+        return books_list
