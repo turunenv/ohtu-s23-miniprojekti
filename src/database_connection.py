de1aeb226ec565a319fa65bde4895825690fb323
turunenv <turunenv@outlook.com>
2023-11-22 14:57:31 +0200

    building the initial database works

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,6 +1,7 @@
 import sqlite3
 from config import DATABASE_FILE_PATH
 
+print(DATABASE_FILE_PATH)
 connection = sqlite3.connect(DATABASE_FILE_PATH)
 connection.row_factory = sqlite3.Row
 
--- src/initialize_database.py
+++ src/initialize_database.py
@@ -1,1 +1,37 @@
-###placeholder
+from database_connection import get_db_connection
+
+def drop_tables(connection):
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        drop table if exists book_references
+    """)
+
+    connection.commit()
+
+def create_tables(connection):
+    """
+    Here we create the tables for all the supported reference types
+    """
+
+    cursor = connection.cursor()
+
+    # create the book_references table
+    cursor.execute("""
+        create table book_references (
+            ref_key text primary key,
+            author text,
+            title text,
+            year integer,
+            publisher text
+        )
+    """)
+
+    connection.commit()
+
+def initialize_database():
+    connection = get_db_connection()
+
+    drop_tables(connection)
+    create_tables(connection)
+
