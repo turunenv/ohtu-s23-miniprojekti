d036a2d147089f7638483580f1fe2bda1326defd
sebastian-lehto <sebastian.lehto@gmail.com>
2023-11-22 18:56:50 +0200

    koodin siistimistä

--- src/app.py
+++ src/app.py
@@ -9,39 +9,30 @@ class App:
 
         while True:
             
-            # Kysytään käyttäjältä komentoa
             command = self.io.read("Command (add or list)")
 
             if not command:
                 break
 
-            # Lisätään viite
             if command == "add":
                 self.add_reference()
             
-            # Listataan viitteet
             if command == "list":
                 self.list_references()
 
     def add_reference(self):
 
-        # Kysytään viitteen tyyppiä
         source_type = self.io.read("Give source type: ")
 
-        # Haetaan lista viitetyypin pakollisista kentistä
         list_of_fields = self.reference_service.get_fields_of_reference_type(source_type)
         
-        # Jos viitetyyppiä ei löydy serviceltä, ilmoitetaan käyttäjälle ja aloitetaan alusta
         if not list_of_fields:
             self.io.write("ERROR: Source type not supported!")
             return
         
-        # Alustetaan dictionary, johon viitteen tiedot talletetaan
-        # Viitetyyppi lisätään
         rd = {}
         rd["type"] = source_type
 
-        # Luetaan käyttäjän syöte jokaiseen vaadittuun kenttään
         for f in list_of_fields:
 
             input = self.io.read(f"Add {f} of the {source_type}: ")
@@ -50,21 +41,18 @@ class App:
                 self.io.write("This ref_key is already taken!!")
                 input = self.io.read(f"Add {f} of the {source_type}: ")
 
-            # Validoidaan syöte
             while input.strip() == "":
                 self.io.write("This field is required!")
                 input = self.io.read(f"Add {f} of the {source_type}: ")
 
             rd[f] = input
 
-        # Lähetetään viite servicen avulla tietokantaan
         if self.reference_service.create_reference(rd):
             self.io.write("ADDED!")
 
 
     def list_references(self):
 
-        # Haetaan kaikki talletetut viitteet serviceltä tulostetaan ne komentoriville
         self.list = self.reference_service.get_all()
         for r in self.list:
             self.io.write(r)
