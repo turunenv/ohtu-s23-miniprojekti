21ba0b9b3ea7b49c9a57a54a3d83d73a6433387b
Niko Raulas <niko.raulas@outlook.com>
2023-11-26 16:58:22 +0200

    add tests to reference_repository

--- src/tests/reference_repository_test.py
+++ src/tests/reference_repository_test.py
@@ -34,10 +34,42 @@ class TestReferenceRepository(unittest.TestCase):
         self.assertEqual(expected_book.year, self.test_book.year)
         self.assertEqual(expected_book.publisher, self.test_book.publisher)
 
-    def test_delet_all_books_works(self):
+    def test_delete_all_books_works(self):
         repository.create_book(self.test_book)
         repository.delete_all_books()
 
         books = repository.get_all()
 
         self.assertEqual(len(books), 0)
+
+    def test_delete_book_with_valid_ref_key(self):
+        repository.create_book(self.test_book)
+        success = repository.delete_book_by_ref_key("1")
+
+        self.assertTrue(success)
+
+    def test_delete_book_with_invalid_ref_key(self):
+        success = repository.delete_book_by_ref_key("1")
+
+        self.assertFalse(success)
+
+    def test_get_book_by_ref_key_creates_correct_book_class(self):
+        repository.create_book(self.test_book)
+        fetched_book = repository.get_book_by_ref_key("1")
+
+        ref_key = fetched_book.ref_key
+        author = fetched_book.author
+        title = fetched_book.title
+        year = fetched_book.year
+        publisher = fetched_book.publisher
+
+        self.assertEqual(ref_key, self.test_book.ref_key)
+        self.assertEqual(author, self.test_book.author)
+        self.assertEqual(title, self.test_book.title)
+        self.assertEqual(year, self.test_book.year)
+        self.assertEqual(publisher, self.test_book.publisher)
+
+    def test_get_book_returns_none_if_book_not_foun(self):
+        fetched_book = repository.get_book_by_ref_key("1")
+
+        self.assertIsNone(fetched_book)
