89c70f18307efc4c60a6de2640aaeae67334e9b7
Niko Raulas <niko.raulas@outlook.com>
2023-12-10 15:09:54 +0200

    add tests for tags

--- src/tests/reference_service_test.py
+++ src/tests/reference_service_test.py
@@ -7,6 +7,7 @@ from services.reference_service import ReferenceService
 class FakeReferenceRepository:
     def __init__(self):
         self.references = []
+        self.tag_names = ["test_tag"]
 
     def get_all(self):
         return self.references
@@ -26,6 +27,25 @@ class FakeReferenceRepository:
         return True
 
     def delete_book_by_ref_key(self, ref_key):
+        return True
+
+    def get_tagged(self, tag_id):
+        return True
+
+    def get_tag_id(self, tag_name):
+        if tag_name in self.tag_names:
+            return 1
+
+        return None
+
+    def create_tag(self, tag_name):
+        if tag_name == "not_a_tag":
+            return False
+
+        self.tag_names.append(tag_name)
+        return True
+
+    def create_tag_relation(self, tag_key, ref_key):
         return True
 
 
@@ -103,3 +123,44 @@ class TestReferenceService(unittest.TestCase):
         taken = self.reference_service.ref_key_taken("KEY")
 
         self.assertTrue(taken)
+
+    def test_add_tag_relation_error_if_ref_key_not_exists(self):
+        error = self.reference_service.add_tag_relation("test_tag", "da")
+        self.assertFalse(error[0])
+        self.assertEqual(error[1], "Not an existing reference")
+
+    def test_add_tag_relation_with_existing_tag_works(self):
+        self.reference_service.create_reference(self.test_article)
+        return_value  = self.reference_service.add_tag_relation("test_tag", "abc")
+        
+        self.assertTrue(return_value[0])
+        self.assertEqual(return_value[1], "Existing tag")
+
+    def test_add_tag_relation_with_non_existant_tag_fails(self):
+        self.reference_service.create_reference(self.test_book)
+        return_value = self.reference_service.add_tag_relation("not_a_tag", "KEY")
+
+        self.assertFalse(return_value[0])
+        self.assertEqual(return_value[1], "Something went wrong with creating the tag")
+
+    def test_add_tag_relation_with_non_existant_tag_works(self):
+        self.reference_service.create_reference(self.test_article)
+        return_value = self.reference_service.add_tag_relation("new_tag", "abc")
+
+        self.assertTrue(return_value[0])
+        self.assertEqual(return_value[1], "New tag created")
+
+    def test_get_tag_id_returns_none(self):
+        return_value = self.reference_service.get_tag_id("NA")
+
+        self.assertFalse(return_value[0])
+        self.assertEqual(return_value[1], "Not an existing tag")
+
+    def test_get_tag_id_returns_id(self):
+        return_value = self.reference_service.get_tag_id("test_tag")
+
+        self.assertTrue(return_value[0])
+        self.assertEqual(return_value[1], 1)
+
+    def test_get_tagged_call_works(self):
+        self.assertTrue(self.reference_service.get_tagged(1))
